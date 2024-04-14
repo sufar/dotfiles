9d63f960429796b5fbd8dfdcaa788bed0887ffb7
sufar <fozoto@163.com>
2024-04-14 20:49:38 +0800

    upgrade to dwm 6.5

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -109,6 +109,7 @@ static const Layout layouts[] = {
 /* commands */
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 //static const char *dmenucmd[] = { "rofi", "-show", "drun", NULL };
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "rofi", "-no-lazy-grab","-show", "drun", "-modi", "drun", "-show-icons", NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *volup[]  = { "/home/zugle/.config/dwm/scripts/volup.sh", NULL };
